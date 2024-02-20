201005894d367aa05702aa12ddfa3fd01855756b
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-21 00:19:30 +0300

    feat: adds section hero , categories for page home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import Categories from '../components/pages/home/categories/Categories';
+import Hero from '../components/pages/home/hero/Hero';
+
 const Home = () => {
-  return <></>
+  return (
+    <>
+      <Hero />
+      <Categories />
+    </>
+  );
 };
 
 export default Home;
