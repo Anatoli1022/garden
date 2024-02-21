66ee4ea2b32c48146be5a6f141c913c0e2133653
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-22 02:07:25 +0300

    feat: adds section order for page home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Categories from '../components/pages/home/categories/Categories';
 import Hero from '../components/pages/home/hero/Hero';
+import Order from '../components/pages/home/order/Order';
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Categories />
+      <Order />
     </>
   );
 };
