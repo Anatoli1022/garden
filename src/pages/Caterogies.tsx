7411467f1e730cd714eef6f39cf5a456437b60a4
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-23 22:49:24 +0300

    feat: adds page categories , adds contact for page home

--- src/pages/Caterogies.tsx
+++ src/pages/Caterogies.tsx
@@ -1,17 +1,13 @@
 import Categories from '../components/shared/categories/Categories';
-import Hero from '../components/pages/home/hero/Hero';
-import Order from '../components/pages/home/order/Order';
 import Contact from '../components/shared/contact/Contact';
 
-const Home = () => {
+const CaterogiesPage = () => {
   return (
     <>
-      <Hero />
       <Categories />
-      <Order />
       <Contact />
     </>
   );
 };
 
-export default Home;
+export default CaterogiesPage;
