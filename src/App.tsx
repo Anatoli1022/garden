e212db526db5e8e020c1f4c73c5346630f035335
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-27 23:53:27 +0300

    feat: adds new routes for app and fix names

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './layouts/layout';
 import Home from './pages/Home';
+import Caterogies from './pages/Caterogies';
+import Discounted from './pages/Discounted'
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="categories" element={<Caterogies />} />
+          <Route path="discounted" element={<Discounted />} />
+          <Route path="notFound" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
