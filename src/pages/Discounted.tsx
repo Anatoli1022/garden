b081de0899a4dc484542df1e9785e7b8e39f4caf
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-27 23:53:03 +0300

    feat: adds page Discounted

---
+++ src/pages/Discounted.tsx
@@ -0,0 +1,13 @@
+import Sales from '../components/pages/discounted/sales/Sales';
+import Contact from '../components/shared/contact/Contact';
+
+const Discounted = () => {
+  return (
+    <>
+      <Sales />
+      <Contact />
+    </>
+  );
+};
+
+export default Discounted;
