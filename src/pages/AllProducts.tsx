b42512047518049be36a7e16bc02c3dac9fa2b24
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-02 21:24:48 +0300

    feat: adds page allProducts

---
+++ src/pages/AllProducts.tsx
@@ -0,0 +1,13 @@
+import Products from '../components/pages/allProducts/products/Products';
+import Contact from '../components/shared/contact/Contact';
+
+const AllProducts = () => {
+  return (
+    <>
+      <Products />
+      <Contact />
+    </>
+  );
+};
+
+export default AllProducts;
