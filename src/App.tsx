68fdc6350fdd08f4d3dcacbe9db0c16ae766d1e1
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-02 21:26:32 +0300

    feat: adds new pages in routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/layout';
 import Home from './pages/Home';
 import Caterogies from './pages/Caterogies';
-import Discounted from './pages/Discounted'
+import Discounted from './pages/Discounted';
 import NotFound from './pages/NotFound';
+import AllProducts from './pages/AllProducts';
+import SingleProduct from './pages/SingleProduct';
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
           <Route path="categories" element={<Caterogies />} />
           <Route path="discounted" element={<Discounted />} />
           <Route path="notFound" element={<NotFound />} />
+          <Route path="allProducts" element={<AllProducts />} />
+          <Route path="allProducts/:slug" element={<SingleProduct />} />
         </Route>
       </Routes>
     </BrowserRouter>
