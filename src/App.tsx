81b53f0d03ae8092148e3a6ca17939a482def219
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-09 00:00:03 +0300

    feat: adds new page in app and fix categories name

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './layouts/layout';
 import Home from './pages/Home';
-import Caterogies from './pages/Caterogies';
+import Categories from './pages/Categories';
 import Discounted from './pages/Discounted';
 import NotFound from './pages/NotFound';
 import AllProducts from './pages/AllProducts';
+import ShoppingCart from './pages/ShoppingCart';
 import SingleProduct from './pages/SingleProduct';
 
 const App = () => {
@@ -14,10 +15,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="categories" element={<Caterogies />} />
+          <Route path="categories" element={<Categories />} />
           <Route path="discounted" element={<Discounted />} />
           <Route path="notFound" element={<NotFound />} />
           <Route path="allProducts" element={<AllProducts />} />
+          <Route path="shoppingCart" element={<ShoppingCart />} />
           <Route path="allProducts/:slug" element={<SingleProduct />} />
         </Route>
       </Routes>
