b6f9be8a0f208ba30d05a5b267e7a29f0b95559c
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-09 00:03:05 +0300

    feat: adds store and provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/main.scss';
-
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 // import 'normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
