97ae251c9de7427128f48a61232badec6472e718
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-09 00:00:40 +0300

    feat: adds basket in header and new links

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -4,43 +4,50 @@ import styles from './Header.module.scss';
 import logo from '../../../assets/logo.svg';
 import basket from '../../../assets/basket.svg';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../../hooks';
 
 const cx = classNames.bind(styles);
 const Header = () => {
+  const itemsBasket = useAppSelector((state) => state.basket.products);
+
   return (
     <header className={cx('header')}>
       <div className={cx('container')}>
         <nav className={cx('navigation')}>
           <div>
-            <Link to="">
-              <img src={logo} alt="" className={cx('logo')} />
+            <Link to="/">
+              <img src={logo} alt="" className={cx('logo')} loading="eager" />
             </Link>
           </div>
           <ul className={cx('list')}>
             <li>
-              <Link to="" className={cx('link')}>
+              <Link to="/" className={cx('link')}>
                 Main Page
               </Link>
             </li>
             <li>
-              <Link to="" className={cx('link')}>
+              <Link to="/categories" className={cx('link')}>
                 Categories
               </Link>
             </li>
             <li>
-              <Link to="" className={cx('link')}>
+              <Link to="/allProducts" className={cx('link')}>
                 All products
               </Link>
             </li>
             <li>
-              <Link to="" className={cx('link')}>
+              <Link to="/discounted" className={cx('link')}>
                 All sales
               </Link>
             </li>
           </ul>
           <div>
-            <Link to="">
-              <img src={basket} alt="" className={cx('basket')} />
+            <Link to="/shoppingCart" className={cx('basket')}>
+              {itemsBasket.length > 0 ? (
+                <span className={cx('items-basket')}>{itemsBasket.length}</span>
+              ) : null}
+
+              <img src={basket} alt="" loading="eager" />
             </Link>
           </div>
         </nav>
