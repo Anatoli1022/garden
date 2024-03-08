26895e463814d3e62c6ae7463230c0a64d198df4
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-08 23:56:47 +0300

    feat: adds page shoppingCart

---
+++ src/pages/ShoppingCart.tsx
@@ -0,0 +1,13 @@
+import Hero from '../components/pages/shoppingCart/hero/Hero';
+import Contact from '../components/shared/contact/Contact';
+
+const ShoppingCart = () => {
+  return (
+    <>
+      <Hero />
+      <Contact />
+    </>
+  );
+};
+
+export default ShoppingCart;
