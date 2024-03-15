57764e3e33e1939565202c5f0eb56a44ded22c35
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-15 19:07:09 +0300

    feat: adds new logic for basket

--- src/redux/basket/basket.tsx
+++ src/redux/basket/basket.tsx
@@ -5,16 +5,53 @@ const initialState: BasketState = {
   products: [],
 };
 
+interface IncreaseQuantityPayload {
+  id: number;
+}
+
+interface DecreaseQuantityPayload {
+  id: number;
+}
+
 const basketReducer = createSlice({
   name: 'basket',
   initialState,
   reducers: {
-    setAddItem: (state, action:PayloadAction<myData>) => {
-      state.products = [...state.products, action.payload];
+    setAddItem: (state, action: PayloadAction<myData>) => {
+      const itemInCart = state.products.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemInCart) {
+        if (itemInCart.quantity !== undefined) {
+          itemInCart.quantity++;
+          console.log(itemInCart.quantity);
+        }
+      } else {
+        state.products.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    increaseQuantity: (
+      state,
+      action: PayloadAction<IncreaseQuantityPayload>
+    ) => {
+      const item = state.products.find((item) => item.id === action.payload.id);
+      if (item && item.quantity !== undefined) {
+        item.quantity++;
+      }
+    },
+    decreaseQuantity: (
+      state,
+      action: PayloadAction<DecreaseQuantityPayload>
+    ) => {
+      const item = state.products.find((item) => item.id === action.payload.id);
+      if (item && item.quantity !== undefined && item.quantity > 1) {
+        item.quantity--;
+      }
     },
   },
 });
 
-export const { setAddItem } = basketReducer.actions;
+export const { setAddItem, increaseQuantity,decreaseQuantity } = basketReducer.actions;
 
 export default basketReducer.reducer;
